9a5fe64e24e621e6b63deb16ffc5c9d5a9cf6c30
Daniel Kalotay <d.kalotay@gmail.com>
2012-06-01 22:44:30 +0200

    hide unused elements depending on open status

--- main.js
+++ main.js
@@ -1,47 +1,53 @@
-    <script language="javascript">
+var xhr = new XMLHttpRequest();
 
-    var xhr = new XMLHttpRequest();
+xhr.open("GET", "https://fixme.ch/cgi-bin/spaceapi.py", true);
 
-    xhr.open("GET", "https://fixme.ch/cgi-bin/spaceapi.py", true);
+xhr.onreadystatechange = function(){
 
-    xhr.onreadystatechange = function(){
+  if ( xhr.readyState == 4 ) {
 
-      if ( xhr.readyState == 4 ) {
+    parse_text = jsonParse(xhr.responseText);
 
-        parse_text = jsonParse(xhr.responseText);
-
-        if ( xhr.status == 200 ) {
-
-          document.body.innerHTML = parse_text.status;
-
+    if ( xhr.status == 200 ) {
+        isOpen = parse_text.open;
+        if (isOpen) {
+            closeBlock = document.getElementById("close-block");
+            closeBlock.style.visibility = "hidden";
         } else {
-
-          document.body.innerHTML = "ERROR";
-
+            openBlock = document.getElementById("open-block");
+            openBlock.style.visibility = "hidden";
         }
 
-      }
 
-    };
+    } else {
 
-    xhr.send(null);
+      document.body.innerHTML = "ERROR";
 
-    function openSpace(hoursOpen) {
-        hoursOpen = Math.floor(hoursOpen);
-        if (isNaN(hoursOpen) || hoursOpen < 0) {
-            //TODO early return or throw exception
-        }
-        var requestUrl = baseUrl + "?do=custom&hours=" + hoursOpen;
-        var requestObject = new XMLHttpRequest();
-        requestObject.open("GET", requestUrl, true);
-        requestObject.send(null);
     }
 
-    function closeSpace() {
-        var requestUrl = baseUrl + "?do=close";
-        var requestObject = new XMLHttpRequest();
-        requestObject.open("GET", requestUrl, true);
-        requestObject.send(null);
-    }
+  }
 
-    </script>
+};
+
+xhr.send(null);
+
+//TODO get twitter url
+function openSpace() {
+    hoursOpen = document.hoursform.hours.value;
+    hoursOpen = Math.floor(hoursOpen);
+    if (isNaN(hoursOpen) || hoursOpen < 0) {
+        //TODO early return or throw exception
+    }
+    var requestUrl = baseUrl + "?do=custom&hours=" + hoursOpen;
+    var requestObject = new XMLHttpRequest();
+    requestObject.open("GET", requestUrl, true);
+    requestObject.send(null);
+}
+
+function closeSpace() {
+    var requestUrl = baseUrl + "?do=close";
+    var requestObject = new XMLHttpRequest();
+    requestObject.open("GET", requestUrl, true);
+    requestObject.send(null);
+}
+
