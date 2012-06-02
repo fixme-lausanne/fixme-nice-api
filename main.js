8f6b75600d13f6d90232f18767290442c9c2f6fd
gcmalloc <gcmalloc@gmail.com>
2012-06-02 14:19:03 +0200

    cleaning code

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 function onPageLoad() {
-    //add the refresh every 5 minutes
-    updateSpaceInformation()
+    //TODO add the refresh every 5 minutes
+    updateSpaceInformation();
 }
 
+var apiUrl = "https://fixme.ch/cgi-bin/spaceapi.py";
 function updateSpaceInformation() {
     var xhr = new XMLHttpRequest();
 
@@ -10,16 +11,16 @@ function updateSpaceInformation() {
 
     xhr.onreadystatechange = function() {
 
-      if ( xhr.readyState == 4 ) {
-        try {
-            var parsed_text = jsonParse(xhr.responseText);
-        } catch (err) {
-            //json parsing failed
-            document.body.innerHTML = "ERROR";
-            return
-        }
-        if ( xhr.status == 200 ) {
-            var isOpen = parsed_text.open;
+      if ( xhr.readyState == 4) {
+        if (xhr.status == 200) {
+            try {
+                var parsed_text = jsonParse(xhr.responseText);
+                var isOpen = parsed_text.open;
+            } catch (err) {
+                //json parsing failed or doesn't contain the correct element
+                displayError();
+                return;
+            }
             var closeBlock = document.getElementById("close-block");
             var openBlock = document.getElementById("open-block");
             if (isOpen) {
@@ -29,21 +30,19 @@ function updateSpaceInformation() {
                 openBlock.style.visibility = "visible";
                 closeBlock.style.visibility = "hidden";
             }
-
         } else {
-
-          document.body.innerHTML = "ERROR";
-
+            displayError();
         }
-
       }
-
     };
-
     xhr.send(null);
 }
 
-var baseUrl = "https://fixme.ch/cgi-bin/twitter.pl"
+function displayError() {
+    document.body.innerHTML = "ERROR";
+}
+
+var baseUrl = "https://fixme.ch/cgi-bin/twitter.pl";
 function openSpace() {
     var hoursForm = document.hoursform.hours;
     var hoursOpen = hoursForm.value;
