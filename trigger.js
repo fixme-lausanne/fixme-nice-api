3f4d2fe55a2d9225e73d8857804885cf69f56e9e
dgellow <samuel.elborai@gmail.com>
2015-06-04 02:50:44 +0200

    some cleaning
    - wrap the code into an anonymous function
    - remove redundant 'use strict' statements
    - add missing semicolons

--- trigger.js
+++ trigger.js
@@ -1,112 +1,124 @@
 /*jslint  browser:true*/
-/*global  jsonParse*/
-/*global  confirm*/
 
-var closing_time;
-var url_twitter = "https://fixme.ch/cgi-bin/twitter.pl";
-var url_api = "https://fixme.ch/status.json";
-var url_led = "http://led.fixme.ch/rgb/";
+(function($, Konami) {
+    'use strict';
 
-var updateBlock=  $("#update-block");
-var msgBlock   =  $("#msg-block");
-var loadBlock  =  $("#loading-block");
-var closeBlock =  $("#close-block");
-var openBlock  =  $("#open-block");
-var slider     =  $("#breathSlider");
+    var closing_time;
+    var url_twitter = "https://fixme.ch/cgi-bin/twitter.pl";
+    var url_api = "https://fixme.ch/status.json";
+    var url_led = "http://led.fixme.ch/rgb/";
 
-function displayError(err) {
-    "use strict";
-    loadBlock.hide();
-    msgBlock.text("Error connecting to fixme server: "+ err);
-}
+    var updateBlock=  $("#update-block");
+    var msgBlock   =  $("#msg-block");
+    var loadBlock  =  $("#loading-block");
+    var closeBlock =  $("#close-block");
+    var openBlock  =  $("#open-block");
+    var slider     =  $("#breathSlider");
 
-function checkHours() {
-    "use strict";
-    var hoursForm = $('#hours');
-    var hoursOpen = Math.floor(hoursForm.val());
-    var openButton = $('#btn-open');
-    if (isNaN(hoursOpen) || hoursOpen < 1) {
-        openButton.attr('disabled', 'disabled');
-    } else {
-        openButton.removeAttr('disabled');
-    }
-}
-
-function changeHour(inc) {
-    "use strict";
-    var hours = $("#hours");
-    if (inc) {
-        hours.val(parseInt(hours.val(), 10) + 1);
-    } else if (hours.val() > 1) {
-        hours.val(parseInt(hours.val(), 10) - 1);
-    }
-    checkHours();
-}
-
-function pad(n) {
-    "use strict";
-    return ("0" + n).slice(-2);
-}
-
-function updateSpaceInformation() {
-    "use strict";
-    loadBlock.show();
-
-    $.getJSON(url_api, function(data){
-        var isOpen = data.state.open;
-        var open_duration = data.state.ext_duration;
-        closing_time = new Date(data.state.lastchange * 1000);
-        closing_time.setHours(closing_time.getHours() + open_duration);
-        update_date(calcDiff(closing_time))
-
-        if (isOpen) {
-            openBlock.hide();
-            closeBlock.show();
-        } else {
-            openBlock.show();
-            closeBlock.hide();
-            document.hoursform.hours.focus();
-        }
-        updateBlock.html('Last update:<br/>'
-            + closing_time.toDateString()
-            + ' ' + pad(closing_time.getHours())
-            + ':' + pad(closing_time.getMinutes()));
-        msgBlock.html(data.state.message);
+    function displayError(err) {
         loadBlock.hide();
+        msgBlock.text("Error connecting to fixme server: "+ err);
+    }
+
+    function checkHours() {
+        var hoursForm = $('#hours');
+        var hoursOpen = Math.floor(hoursForm.val());
+        var openButton = $('#btn-open');
+        if (isNaN(hoursOpen) || hoursOpen < 1) {
+            openButton.attr('disabled', 'disabled');
+        } else {
+            openButton.removeAttr('disabled');
+        }
+    }
+
+    function changeHour(inc) {
+        var hours = $("#hours");
+        if (inc) {
+            hours.val(parseInt(hours.val(), 10) + 1);
+        } else if (hours.val() > 1) {
+            hours.val(parseInt(hours.val(), 10) - 1);
+        }
         checkHours();
-    });
-}
-
-function calcDiff() {
-    "use strict";
-    if(closing_time == undefined) return new Date(0);
-    var diff_time = Number(closing_time.getTime()) - new Date().getTime();
-    if (diff_time > 0) {
-        return new Date(diff_time);
-    } else {
-        return new Date(0);
     }
-}
 
-function launchAutoRefresh() {
-    "use strict";
-    setInterval(function () {
-        update_date(calcDiff(closing_time))
-    }, 300);
-}
-
-function openSpace(extend) {
-    "use strict";
-    var hoursOpen = document.hoursform.hours.value;
-    if (hoursOpen === undefined || extend) {
-        hoursOpen = prompt("Hours:", 1);
-        if (hoursOpen === null) return;
+    function pad(n) {
+        return ("0" + n).slice(-2);
     }
-    var confirm_return = confirm("Are you sure you want to open the hackerspace ?");
-    if (confirm_return) {
-        msgBlock.html("Opening space...");
-        var requestUrl = url_twitter + "?do=custom&hours=" + hoursOpen,
-            requestObject = new XMLHttpRequest();
+
+    function updateSpaceInformation() {
+        loadBlock.show();
+
+        $.getJSON(url_api, function(data){
+            var isOpen = data.state.open;
+            var open_duration = data.state.ext_duration;
+            closing_time = new Date(data.state.lastchange * 1000);
+            closing_time.setHours(closing_time.getHours() + open_duration);
+            update_date(calcDiff(closing_time));
+
+            if (isOpen) {
+                openBlock.hide();
+                closeBlock.show();
+            } else {
+                openBlock.show();
+                closeBlock.hide();
+                document.hoursform.hours.focus();
+            }
+
+            updateBlock.html('Last update:<br/>'.concat(
+                closing_time.toDateString(), ' ',
+                pad(closing_time.getHours()), ':',
+                pad(closing_time.getMinutes())
+            ));
+
+            msgBlock.html(data.state.message);
+            loadBlock.hide();
+            checkHours();
+        });
+    }
+
+    function calcDiff() {
+        if(closing_time === undefined) return new Date(0);
+        var diff_time = Number(closing_time.getTime()) - new Date().getTime();
+        if (diff_time > 0) {
+            return new Date(diff_time);
+        } else {
+            return new Date(0);
+        }
+    }
+
+    function launchAutoRefresh() {
+        setInterval(function () {
+            update_date(calcDiff(closing_time));
+        }, 300);
+    }
+
+    function openSpace(extend) {
+        var hoursOpen = document.hoursform.hours.value;
+        if (hoursOpen === undefined || extend) {
+            hoursOpen = prompt("Hours:", 1);
+            if (hoursOpen === null) return;
+        }
+        var confirm_return = confirm("Are you sure you want to open the hackerspace ?");
+        if (confirm_return) {
+            msgBlock.html("Opening space...");
+            var requestUrl = url_twitter + "?do=custom&hours=" + hoursOpen,
+                requestObject = new XMLHttpRequest();
+            $.ajax(requestUrl, {
+                type: 'GET',
+                complete: function(data){
+                    msgBlock.html(data.responseText);
+                },
+            });
+        }
+    }
+
+    function closeSpace() {
+        var confirm_value = confirm("Are you sure you want to close the space ?");
+        if (!confirm_value) {
+            return;
+        }
+        msgBlock.text("Closing space...");
+        var requestUrl = url_twitter + "?do=close";
         $.ajax(requestUrl, {
             type: 'GET',
             complete: function(data){
@@ -114,148 +126,122 @@ function openSpace(extend) {
             },
         });
     }
-}
 
-function closeSpace() {
-    "use strict";
-    var confirm_value = confirm("Are you sure you want to close the space ?");
-    if (!confirm_value) {
-        return;
+    function update_date(date) {
+        var hours = String(date.getHours() - 1); // Time Zone GMT-1
+        if (hours.length == 1) {
+            hours = "0" + hours;
+        }
+        $("#hour").text(hours);
+
+        var minutes = String(date.getMinutes());
+        if (minutes.length == 1) {
+            minutes = "0" + minutes;
+        }
+        $("#minute").text(minutes);
+
+        var seconds = String(date.getSeconds());
+        if (seconds.length == 1) {
+            seconds = "0" + seconds;
+        }
+        $("#second").text(seconds);
     }
-    msgBlock.text("Closing space...");
-    var requestUrl = url_twitter + "?do=close";
-    $.ajax(requestUrl, {
-        type: 'GET',
-        complete: function(data){
-            msgBlock.html(data.responseText);
-        },
-    });
-}
 
-function update_date(date) {
-    "use strict";
-    var hours = String(date.getHours() - 1); // Time Zone GMT-1
-    if (hours.length == 1) {
-        hours = "0" + hours;
+    function switchTheLight(red, green, blue) {
+        $.ajax(url_led, {
+            type: 'POST',
+            data: {red: red, green: green, blue: blue, breathe: slider.val()},
+        });
     }
-    $("#hour").text(hours);
 
-    var minutes = String(date.getMinutes());
-    if (minutes.length == 1) {
-        minutes = "0" + minutes;
+    function changeBreathSpeed(){
+        $('#breathValue').text(slider.val() + ' ms');
     }
-    $("#minute").text(minutes);
 
-    var seconds = String(date.getSeconds());
-    if (seconds.length == 1) {
-        seconds = "0" + seconds;
+    function setTheBreathSpeed() {
+        $.ajax(url_led, {
+            type: 'POST',
+            data: {breathe: slider.val()},
+        });
     }
-    $("#second").text(seconds);
-}
 
-function switchTheLight(red, green, blue) {
-    "use strict";
-    $.ajax(url_led, {
-        type: 'POST',
-        data: {red: red, green: green, blue: blue, breathe: slider.val()},
-    });
-}
-
-function changeBreathSpeed(){
-    "use strict";
-    $('#breathValue').text(slider.val() + ' ms');
-}
-
-function setTheBreathSpeed() {
-    "use strict";
-    $.ajax(url_led, {
-        type: 'POST',
-        data: {breathe: slider.val()},
-    });
-}
-
-function switchTheLightColor(color) {
-    "use strict";
-    Police.switchOff();
-    switch(color) {
+    function switchTheLightColor(color) {
+        Police.switchOff();
+        switch(color) {
         case 'red':
-            switchTheLight(255, 0, 0)
+            switchTheLight(255, 0, 0);
             break;
         case 'green':
-            switchTheLight(0, 255, 0)
+            switchTheLight(0, 255, 0);
             break;
         case 'blue':
-            switchTheLight(0, 0, 255)
+            switchTheLight(0, 0, 255);
             break;
-    }
-}
-
-function switchTheLightOn() {
-    "set strict";
-    switchTheLight(255, 255, 255);
-}
-
-function switchTheLightOff() {
-    "set strict";
-    Police.switchOff();
-    switchTheLight(0, 0, 0);
-}
-
-function switchTheLightToPolice() {
-    "use strict";
-    Police.switchOn()
-}
-
-var Police = {
-    policeEvent: 0,
-    delay: 100,
-    switchOff: function switchOff() {
-        window.clearInterval(Police.policeEvent);
-        Police.policeEvent = 0;
-    },
-    switchOn: function switchOn(red) {
-        if(Police.policeEvent != 0) {
-            if (red) {
-                switchTheLight(255, 0, 0);
-            } else {
-                switchTheLight(0, 0, 255);
-            }
         }
-        Police.policeEvent = window.setTimeout(function() { Police.switchOn(!red)}, Police.delay);
-    },
-};
+    }
 
-$(document).ready(function(){
-    "use strict";
+    function switchTheLightOn() {
+        switchTheLight(255, 255, 255);
+    }
 
-    msgBlock.show();
-    updateBlock.show();
+    function switchTheLightOff() {
+        Police.switchOff();
+        switchTheLight(0, 0, 0);
+    }
 
-    // Leds buttons
-    $('#btn-red').click(function(){ switchTheLightColor('red'); });
-    $('#btn-green').click(function(){ switchTheLightColor('green'); });
-    $('#btn-blue').click(function(){ switchTheLightColor('blue'); });
-    $('#btn-white').click(function(){ switchTheLightOn() ; });
-    $('#btn-off').click(function(){ switchTheLightOff(); });
-    $('#btn-police').click(function(){ switchTheLightToPolice(); });
-    $('#breathSlider').mouseup(function(){ setTheBreathSpeed(); });
-    $('#breathSlider').on('change mousemove', function(){ changeBreathSpeed(); });
+    function switchTheLightToPolice() {
+        Police.switchOn();
+    }
 
-    // Open/Close buttons
-    $('#btn-minus').click(function(){ changeHour(0); });
-    $('#btn-plus').click(function(){ changeHour(1); });
-    $('#btn-open').click(function(){ openSpace(); });
-    $('#btn-extend').click(function(){ openSpace(1); });
-    $('#btn-close').click(function(){ closeSpace(); });
-    $('#hours').keyup(function(){ checkHours(); });
+    var Police = {
+        policeEvent: 0,
+        delay: 100,
+        switchOff: function switchOff() {
+            window.clearInterval(Police.policeEvent);
+            Police.policeEvent = 0;
+        },
+        switchOn: function switchOn(red) {
+            if(Police.policeEvent !== 0) {
+                if (red) {
+                    switchTheLight(255, 0, 0);
+                } else {
+                    switchTheLight(0, 0, 255);
+                }
+            }
+            Police.policeEvent =
+                window.setTimeout(function() { Police.switchOn(!red); }, Police.delay);
+        },
+    };
 
-    // Set up trigger
-    launchAutoRefresh();
-    updateSpaceInformation();
-    setInterval(updateSpaceInformation, 60 * 1000);
+    $(document).ready(function(){
+        msgBlock.show();
+        updateBlock.show();
 
-    // Egging
-    var konami = new Konami();
-    konami.load("https://www.youtube.com/watch?v=1Wytn-_MSBo");
-});
+        // Leds buttons
+        $('#btn-red').click(function(){ switchTheLightColor('red'); });
+        $('#btn-green').click(function(){ switchTheLightColor('green'); });
+        $('#btn-blue').click(function(){ switchTheLightColor('blue'); });
+        $('#btn-white').click(function(){ switchTheLightOn() ; });
+        $('#btn-off').click(function(){ switchTheLightOff(); });
+        $('#btn-police').click(function(){ switchTheLightToPolice(); });
+        $('#breathSlider').mouseup(function(){ setTheBreathSpeed(); });
+        $('#breathSlider').on('change mousemove', function(){ changeBreathSpeed(); });
 
+        // Open/Close buttons
+        $('#btn-minus').click(function(){ changeHour(0); });
+        $('#btn-plus').click(function(){ changeHour(1); });
+        $('#btn-open').click(function(){ openSpace(); });
+        $('#btn-extend').click(function(){ openSpace(1); });
+        $('#btn-close').click(function(){ closeSpace(); });
+        $('#hours').keyup(function(){ checkHours(); });
+
+        // Set up trigger
+        launchAutoRefresh();
+        updateSpaceInformation();
+        setInterval(updateSpaceInformation, 60 * 1000);
+
+        // Egging
+        var konami = new Konami();
+        konami.load("https://www.youtube.com/watch?v=1Wytn-_MSBo");
+    });
+})(jQuery, Konami);
